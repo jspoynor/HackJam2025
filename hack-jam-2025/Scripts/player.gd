c06467ff9e7caef04da7e1d2b57eb6990ffbea99
Jackson <152156895+jspoynor@users.noreply.github.com>
2025-11-22 13:51:37 -0500

    Complete player movement

--- hack-jam-2025/Scripts/player.gd
+++ hack-jam-2025/Scripts/player.gd
@@ -1,4 +1,23 @@
-extends Node2D
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
 
 func _physics_process(delta):
-	if Input.is_action_pressed()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var x_direction = Input.get_axis("Player Left", "Player Right")
+	var y_direction = Input.get_axis("Player Up", "Player Down")
+	if x_direction:
+		velocity.x = x_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
